4b28edaf151eba9dceaae6e364364ed0bb9ecd95
Perry Baran <perrybaran@gmail.com>
2022-12-13 11:44:14 +0000

    prettierified

--- scripts/create-database.js
+++ scripts/create-database.js
@@ -1,49 +1,47 @@
-const { Client } = require('pg')
-const path = require('path')
+const { Client } = require('pg');
+const path = require('path');
 
-const envName = process.argv.slice(2)[0]
+const envName = process.argv.slice(2)[0];
 
 const loadEnv = (envName) => {
-  const { NODE_ENV } = process.env
+  const { NODE_ENV } = process.env;
   if (NODE_ENV != 'production') {
-  
-    const envFile = envName === 'test' ? '../.env.test' : '../.env'
-    
+    const envFile = envName === 'test' ? '../.env.test' : '../.env';
+
     require('dotenv').config({
       path: path.join(__dirname, envFile),
-    })
-  
-    const databaseName = process.env.PGDATABASE
-  
-    delete process.env.PGDATABASE
+    });
 
-    return databaseName
+    const databaseName = process.env.PGDATABASE;
+
+    delete process.env.PGDATABASE;
+
+    return databaseName;
   }
-}
+};
 
 const createDatabase = async (databaseName) => {
-  const client = new Client()
+  const client = new Client();
   try {
-    await client.connect()
-  
-    console.log(`Creating ${databaseName} database...`)
-  
-    await client.query(`CREATE DATABASE ${databaseName}`)
-  
-    console.log('Database created!')
-  } catch (err) {
+    await client.connect();
 
+    console.log(`Creating ${databaseName} database...`);
+
+    await client.query(`CREATE DATABASE ${databaseName}`);
+
+    console.log('Database created!');
+  } catch (err) {
     switch (err.code) {
-    case "42P04":
-      console.log('Database already exists!')
-      break
-    default:
-      console.log(err)
+      case '42P04':
+        console.log('Database already exists!');
+        break;
+      default:
+        console.log(err);
     }
   } finally {
-    client.end()
+    client.end();
   }
-}
+};
 
-const databaseName = loadEnv(envName)
-createDatabase(databaseName)
+const databaseName = loadEnv(envName);
+createDatabase(databaseName);

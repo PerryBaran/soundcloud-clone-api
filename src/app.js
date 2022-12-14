fd977008e73b8f7b20a55f01766dbfad20f8be0f
Perry Baran <perrybaran@gmail.com>
2022-12-14 13:03:49 +0000

    add album router and error handling

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const userRouter = require('./routes/user');
+const albumRouter = require('./routes/album');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
 
 const app = express();
 
@@ -8,6 +10,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 app.use('/users', userRouter);
+app.use('/albums', albumRouter);
+
+// needs all 4 parameters defined even if unusead to work
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    if (err.code === 'LIMIT_UNEXPECTED_FILE') {
+      return res.status(400).send({ error: "File is wrong type"});
+    }
+  } 
+  return res.status(400).send({ error: err.message ? `Unexpeted Error: ${err.message}` : 'Unexpected error'});
+});
 
 module.exports = app;

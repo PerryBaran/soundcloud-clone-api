81989b95fd9ecf916071c1bc5a5be8bfa8c7877c
Perry Baran <perrybaran@gmail.com>
2022-12-14 15:44:43 +0000

    add ability to upload to S3 in addAlbum

--- src/controllers/album.js
+++ src/controllers/album.js
@@ -1,7 +1,36 @@
+const { uploadFileToS3 } = require("../aws/uploadS3");
+const { Album, Song } = require('../models');
+
 const addAlbum = async (req, res) => {
-  console.log(req.files);
-  res.status(200).send();
+  const { files } = req;
+  try {
+    let imageRef = null;
+    const songs = [];
+
+    for (const image of files.image) {
+      imageRef = await uploadFileToS3(image);
+    }
+
+    for (const audio of files.audio) {
+      const url = await uploadFileToS3(audio);
+      songs.push({
+        name: audio.originalname.split('.')[0],
+        audioRef: url
+      });
+    }
+
+    const data = {
+      imageRef,
+      songs
+    }
+
+    res.status(200).send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: err.message ? `Error: ${err.message}` : 'Unexpted Error'})
+  }
 };
+
 
 module.exports = {
   addAlbum,

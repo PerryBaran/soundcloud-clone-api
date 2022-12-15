936ada28aedb60bfc9ff5c75d303a6e2bfba4b10
Perry Baran <perrybaran@gmail.com>
2022-12-15 17:28:08 +0000

    added authenticate token middleware

--- src/routes/song.js
+++ src/routes/song.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const songController = require('../controllers/song');
 const storeInMemory = require('../middleware/multer');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/', storeInMemory.single('audio'), songController.create);
+router.post('/', auth.authenticateToken, storeInMemory.single('audio'), songController.create);
 
 module.exports = router;

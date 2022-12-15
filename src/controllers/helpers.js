40865e0200e0fcc7c002fb7f9d3d0c2df926cccb
Perry Baran <perrybaran@gmail.com>
2022-12-15 13:12:12 +0000

    changed error handling to avoid multiple responses

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -11,7 +11,7 @@ const getModel = (model) => {
   return models[model];
 };
 
-const getUserId = async (model, data, res) => {
+const getUserId = async (model, data) => {
   try {
     switch (model) {
       case 'album': {
@@ -27,28 +27,29 @@ const getUserId = async (model, data, res) => {
         return UserId;
       }
       default:
-        throw new Error('cannot find userId');
+        throw new Error('Cannot find User');
     }
   } catch (err) {
-    res.status(404).send({ message: 'Cannot find User' });
+    throw new Error('Cannot find User');
   }
 };
 
 exports.createFile = async (data, file, res, model) => {
   const Model = getModel(model);
+  if (!file) return res.status(400).send({ message: 'file required' });
 
   try {
-    const UserId = await getUserId(model, data, res);
+    const UserId = await getUserId(model, data);
     const key = await s3.uploadFileToS3(file, UserId);
     data.key = key;
     const response = await Model.create(data);
     res.status(200).send(response);
   } catch (err) {
-    console.error(err);
-    res
-      .status(500)
-      .send({
-        message: err.message ? `Error: ${err.message}` : 'Unexpected error',
-      });
+    if (err.message === 'Cannot find User') {
+      return res.status(404).send({ message: err.message });
+    }
+    res.status(500).send({
+      message: err.message ? `Error: ${err.message}` : 'Unexpected error',
+    });
   }
 };

5e5593fdf5df48d87131c48447e4e3e66474c00f
Perry Baran <perrybaran@gmail.com>
2022-12-15 17:27:56 +0000

    added authenticateToken middlewarea and added delete

--- src/routes/album.js
+++ src/routes/album.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const albumController = require('../controllers/album');
 const storeInMemory = require('../middleware/multer');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post('/', storeInMemory.single('image'), albumController.create);
+router.post('/', auth.authenticateToken, storeInMemory.single('image'), albumController.create);
+
+router.delete('/:albumId', auth.authenticateToken, albumController.delete);
 
 module.exports = router;

bc5f73b6f34a1160e04c91453f57435d902f461e
Perry Baran <perrybaran@gmail.com>
2022-12-15 11:36:06 +0000

    edited album and song controllers to use helpers

---
+++ src/controllers/helpers.js
@@ -0,0 +1,49 @@
+const { User, Album, Song } = require('../models');
+const { uploadFileToS3 } = require('../aws/s3');
+
+const getModel = (model) => {
+  const models = {
+    user: User,
+    album: Album,
+    song: Song,
+  };
+
+  return models[model];
+};
+
+const getUserId = async (model, data, res) => {
+  try {
+    switch (model) {
+      case 'album': {
+        const { UserId } = await User.findByPk(data.UserId, {
+          raw: true,
+        });
+        return UserId;
+      }
+      case 'song': {
+        const { UserId } = await Album.findByPk(data.AlbumId, {
+          raw: true,
+        });
+        return UserId;
+      }
+      default: throw new Error('cannot find userId');    
+    }
+  } catch (err) {
+    res.status(404).send({ message: 'Cannot find User'})
+  }
+}
+
+exports.createFile = async (data, file, res, model) => {
+  const Model = getModel(model);
+
+  try {
+    const UserId = await getUserId(model, data, res);
+    const key = await uploadFileToS3(file, UserId);
+    data.key = key;
+    const response = await Model.create(data);
+    res.status(200).send(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.message ? `Error: ${err.message}` : 'Unexpected error'});
+  }
+};
--- src/controllers/song.js
+++ src/controllers/song.js
@@ -2,9 +2,9 @@ const { createFile } = require('./helpers');
 
 exports.create = async (req, res) => {
   const { file, body } = req;
-  
+
   try {
-    await createFile(body, file, res, 'album');
+    await createFile(body, file, res, 'song');
   } catch (err) {
     console.error(err);
   }

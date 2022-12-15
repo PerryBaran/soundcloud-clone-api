56422af485985ce103664bedcb36c8b052658a11
Perry Baran <perrybaran@gmail.com>
2022-12-15 11:37:25 +0000

    album route now only adds albums

--- src/routes/album.js
+++ src/routes/album.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const albumController = require('../controllers/album');
 const storeInMemory = require('../middleware/multer');
-const checkAlbumData = require('../middleware/checkAlbumData');
 
 const router = express.Router();
 
 router.post(
   '/',
-  storeInMemory.fields([
-    {
-      name: 'image',
-      maxCount: 1,
-    },
-    {
-      name: 'audio',
-    },
-  ]),
-  checkAlbumData,
-  albumController.addAlbum
+  storeInMemory.single('image'),
+  albumController.create
 );
 
 module.exports = router;

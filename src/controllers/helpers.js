3ea50c64159fb1ea3f7aed5b1e662a0041933fff
Perry Baran <perrybaran@gmail.com>
2022-12-15 12:31:30 +0000

    edited to allow stubbing of s3 using sinon

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -1,5 +1,5 @@
 const { User, Album, Song } = require('../models');
-const { uploadFileToS3 } = require('../aws/s3');
+const s3 = require('../aws/s3');
 
 const getModel = (model) => {
   const models = {
@@ -26,24 +26,29 @@ const getUserId = async (model, data, res) => {
         });
         return UserId;
       }
-      default: throw new Error('cannot find userId');    
+      default:
+        throw new Error('cannot find userId');
     }
   } catch (err) {
-    res.status(404).send({ message: 'Cannot find User'})
+    res.status(404).send({ message: 'Cannot find User' });
   }
-}
+};
 
 exports.createFile = async (data, file, res, model) => {
   const Model = getModel(model);
 
   try {
     const UserId = await getUserId(model, data, res);
-    const key = await uploadFileToS3(file, UserId);
+    const key = await s3.uploadFileToS3(file, UserId);
     data.key = key;
     const response = await Model.create(data);
     res.status(200).send(response);
   } catch (err) {
     console.error(err);
-    res.status(500).send({ message: err.message ? `Error: ${err.message}` : 'Unexpected error'});
+    res
+      .status(500)
+      .send({
+        message: err.message ? `Error: ${err.message}` : 'Unexpected error',
+      });
   }
 };

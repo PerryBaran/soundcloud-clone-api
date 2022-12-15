5ec6d047448eb84b906908568f15d2c5e491c115
Perry Baran <perrybaran@gmail.com>
2022-12-15 13:12:44 +0000

    updated album tests

--- tests/integration/album.test.js
+++ tests/integration/album.test.js
@@ -7,11 +7,6 @@ const sinon = require('sinon');
 
 describe('/albums', () => {
   const fakeResolve = 'a string';
-  const fakeUserData = {
-    name: 'validName',
-    email: 'valid@email.com',
-    password: 'validPassword',
-  };
   let user;
 
   before(async () => {
@@ -19,7 +14,7 @@ describe('/albums', () => {
       await User.sequelize.sync();
       await Album.sequelize.sync();
     } catch (err) {
-      console.error(err);
+      console.error('that annoying error');
     }
   });
 
@@ -27,6 +22,11 @@ describe('/albums', () => {
     sinon.stub(s3, 'uploadFileToS3').resolves(fakeResolve);
 
     try {
+      const fakeUserData = {
+        name: 'validName',
+        email: 'valid@email.com',
+        password: 'validPassword',
+      };
       user = await User.create(fakeUserData);
     } catch (err) {
       console.error(err);
@@ -37,6 +37,7 @@ describe('/albums', () => {
     sinon.restore();
     try {
       await User.destroy({ where: {} });
+      await Album.destroy({ where: {} });
     } catch (err) {
       console.error(err);
     }
@@ -44,6 +45,7 @@ describe('/albums', () => {
 
   describe('POST /albums', () => {
     let validData;
+    const buffer = Buffer.from('fake data');
 
     beforeEach(() => {
       validData = {
@@ -54,10 +56,8 @@ describe('/albums', () => {
 
     it('creates a new album in the database', async () => {
       try {
-        const buffer = Buffer.from('fake data');
         const { status, body } = await request(app)
           .post('/albums')
-          .set('Content-Type', 'multipart/form-data')
           .field('name', validData.name)
           .field('UserId', validData.UserId)
           .attach('image', buffer, 'fake.png');
@@ -70,6 +70,80 @@ describe('/albums', () => {
         expect(body.UserId).to.equal(validData.UserId);
         expect(body.key).to.equal(fakeResolve);
         expect(newAlbumRecord.name).to.equal(body.name);
+      } catch (err) {
+        throw new Error(err);
+      }
+    });
+
+    it("returns 500 if name field doesn't exist", async () => {
+      try {
+        const { status, body } = await request(app)
+          .post('/albums')
+          .field('UserId', validData.UserId)
+          .attach('image', buffer, 'fake.png');
+
+        expect(status).to.equal(500);
+        expect(body.message).to.equal(
+          'Error: notNull Violation: Must provide an album name'
+        );
+      } catch (err) {
+        throw new Error(err);
+      }
+    });
+
+    it("returns 404 if UserId field doesn't exist", async () => {
+      try {
+        const { status, body } = await request(app)
+          .post('/albums')
+          .field('name', validData.name)
+          .attach('image', buffer, 'fake.png');
+
+        expect(status).to.equal(404);
+        expect(body.message).to.equal('Cannot find User');
+      } catch (err) {
+        throw new Error(err);
+      }
+    });
+
+    it("returns 404 if UserId doesn't match a valid User", async () => {
+      try {
+        const { status, body } = await request(app)
+          .post('/albums')
+          .field('name', validData.name)
+          .field('UserId', 999999)
+          .attach('image', buffer, 'fake.png');
+
+        expect(status).to.equal(404);
+        expect(body.message).to.equal('Cannot find User');
+      } catch (err) {
+        throw new Error(err);
+      }
+    });
+
+    it('returns 400 if the image file has the wrong key', async () => {
+      try {
+        const { status, body } = await request(app)
+          .post('/albums')
+          .field('name', validData.name)
+          .field('UserId', validData.UserId)
+          .attach('audio', buffer, 'fake.png');
+
+        expect(status).to.equal(400);
+        expect(body.message).to.equal('File is wrong type');
+      } catch (err) {
+        throw new Error(err);
+      }
+    });
+
+    it("returns 400 if the file doesn't exist", async () => {
+      try {
+        const { status, body } = await request(app)
+          .post('/albums')
+          .field('name', validData.name)
+          .field('UserId', validData.UserId);
+
+        expect(status).to.equal(400);
+        expect(body.message).to.equal('file required');
       } catch (err) {
         throw new Error(err);
       }

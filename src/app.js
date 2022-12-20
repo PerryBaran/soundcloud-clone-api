675af38ad275e86ffa08701bf8693cb4739f540c
Perry Baran <perrybaran@gmail.com>
2022-12-20 17:16:16 +0000

    removed 3000

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: 'https://localhost:3000',
+  origin: 'https://localhost',
   exposedHeaders: ['set-cookie'],
 }));
 

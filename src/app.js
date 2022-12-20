2b6f3076d1db8e9827042f85bac3b74a3841ada9
Perry Baran <perrybaran@gmail.com>
2022-12-20 14:43:16 +0000

    i hate cookies

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: ['https://localhost:3000', 'https://perrybaran.github.io/render-test/'],
+  origin: 'https://localhost:3000',
 }));
 
 app.use('/users', userRouter);

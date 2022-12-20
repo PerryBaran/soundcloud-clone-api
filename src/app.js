d09f7d6dddecb697ace4322c8fa5823fdfa53dd9
Perry Baran <perrybaran@gmail.com>
2022-12-20 17:53:49 +0000

    back to https

--- src/app.js
+++ src/app.js
@@ -13,9 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:3000',
+  origin: 'https://localhost:3000',
   exposedHeaders: ['set-cookie'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use('/users', userRouter);
